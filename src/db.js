49cf77bd1d16710ab3c2628d9064e2c955a2accc
SifaulQolbiSaputra <sipaulalbiol@gmail.com>
2024-06-08 12:16:54 +0700

    bismilah

--- src/db.js
+++ src/db.js
@@ -1,11 +1,17 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+
+// eslint-disable-next-line import/no-extraneous-dependencies
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'SG-ecoswap-db-8744-mysql-master.servers.mongodirector.com',
-  user: 'sgroot',
-  password: 'eVMCmGK#ogtv8fHG',
-  database: 'ecoswap-db',
+  host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+  port: 4000,
+  user: 'GGCZhvFAtPHbqHN.root',
+  password: 'cM07jrMC0fp6JXMT',
+  database: 'test',
+  ssl: {
+    rejectUnauthorized: true, // Sesuaikan dengan kebutuhan Anda
+  },
 });
 
 module.exports = pool.promise();
